aed41656a9ed154f7d3d7daa92ce9d7f01bfbd4a
Sergey <piolersa85@mail.ru>
2021-11-16 16:14:51 +0200

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Sergey on 16.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
